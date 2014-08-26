528129d9047810bd9ab2c0f62ed32e04410d68a7
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-26 08:54:57 +0100

    Add Twig layout

--- Extension.php
+++ Extension.php
@@ -2,6 +2,8 @@
 
 namespace Bolt\Extension\Bolt\RateIt;
 
+use Bolt\Extensions\Snippets\Location as SnippetLocation;
+
 /**
  * RateIt extension for Bolt
  *
@@ -36,7 +38,8 @@ class Extension extends \Bolt\BaseExtension
          * Frontend
          */
         if ($this->app['config']->getWhichEnd() == 'frontend') {
-            //
+            // Twig hook
+            $this->addTwigFunction('rateit', 'twigRateIt');
         }
 
         /*
@@ -45,6 +48,60 @@ class Extension extends \Bolt\BaseExtension
         if ($this->app['config']->getWhichEnd() == 'async') {
             $this->setController();
         }
+    }
+
+    public function twigRateIt($record = null)
+    {
+        // Add our twig path
+        $this->app['twig.loader.filesystem']->addPath(__DIR__ . '/assets/');
+
+        // Add needed JavaScript & CSS
+        $this->addAssets();
+
+        // Get the Twig record object
+        $record = $this->getRecord($record);
+
+        $max = $this->config['stars'];
+        $inc = $this->config['increment'];
+
+        $bolt_record_id = $record->id;
+        $bolt_contenttype = strtolower($record->contenttype['name']);
+
+        if (empty($bolt_record_id) || empty($bolt_contenttype)) {
+            return new \Twig_Markup('<!-- Error: content not found -->', 'UTF-8');
+        }
+
+        // Get the current value of the rating
+        try {
+            $db = new RateItRecords($this->app);
+            $lookup = $db->dbLookupRating(array(
+                'contenttype' => $bolt_contenttype,
+                'record_id' => $bolt_record_id)
+            );
+
+            if(!empty($lookup) && isset($lookup['vote_avg'])) {
+                $current_val = $lookup['vote_avg'];
+            }
+            else {
+                $current_val = 0;
+            }
+        } catch (\Exception $e) {
+            $current_val = 0;
+        }
+
+        // Customisation goes here. See http://rateit.codeplex.com/documentation
+        $html = $this->app['render']->render('rateit.twig', array(
+            'config' => $this->config,
+            'max' => $max,
+            'readonly' => $this->isCookieSet($record),
+            'inc' => $inc,
+            'record' => $record,
+            'bolt_record_id' => $bolt_record_id,
+            'bolt_contenttype' => $bolt_contenttype,
+            'current_val' => $current_val
+        ));
+
+        return new \Twig_Markup($html, 'UTF-8');
     }
 
     /**
@@ -66,6 +123,11 @@ class Extension extends \Bolt\BaseExtension
         else {
             $this->config['px'] = 16;
             $this->config['class'] = '';
+        }
+        if (empty($this->config['tooltips'])) {
+            $this->config['tooltips'] = '';
+        } else {
+            $this->config['tooltips'] = json_encode($this->config['tooltips']);
         }
         if (empty($this->config['reponse_class'])) {
             $this->config['reponse_class'] = '';
@@ -90,6 +152,60 @@ class Extension extends \Bolt\BaseExtension
         $this->app->match("/async/RateIt", array($this->controller, 'ajaxRateIt'))
                     ->bind('ajaxRateIt')
                     ->method('POST');
+    }
+
+    private function addAssets()
+    {
+        // Inject CSS
+        if ($this->config['location'] == 'body') {
+            $this->addCSS($this->config['stylesheet'], true);
+
+        } else {
+            $this->addCSS($this->config['stylesheet']);
+        }
+
+        // JavaScript
+        $this->addJavascript('js/jquery.rateit.min.js', true);
+        $this->addJavascript('js/bolt.rateit.js', true);
+
+        $js = $this->app['render']->render('_javascript.twig', array(
+            'tooltips' => $this->config['tooltips']
+        ));
+
+        $this->insertSnippet(SnippetLocation::END_OF_HTML, $js);
+    }
+
+    private function getRecord($record = null)
+    {
+        if ($record) {
+            return $record;
+        }
+
+        if (isset($this->record)) {
+            return $this->record;
+        }
+
+        $globalTwigVars = $this->app['twig']->getGlobals('record');
+
+        if (isset($globalTwigVars['record'])) {
+            return $globalTwigVars['record'];
+        }
+        else {
+            return false;
+        }
+
+        return $record;
+    }
+
+    private function isCookieSet($record = null)
+    {
+        $bolt_record_id = $record->id;
+        $bolt_contenttype = strtolower( $record->contenttype['name'] );
+
+        if (isset($_COOKIE['rateit'][$bolt_contenttype][$bolt_record_id])) {
+            return true;
+        }
+        return false;
     }
 
 }
