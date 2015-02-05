6d034424cb2d1d2bbdccec504bff26a6ba53458a
Gawain Lynch <gawain.lynch@gmail.com>
2015-02-05 10:09:16 +0000

    Add Grunt to generate JS

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		uglify : {
+			my_target : {
+				options : {
+					sourceMap : true,
+					sourceMapName : 'js/bolt.rateit.min.map',
+					preserveComments : 'some'
+				},
+				files : {
+					'js/bolt.rateit.min.js' : [
+							'js/jquery.rateit.js',
+							'js/init.js' ]
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', [ 'uglify' ]);
+};
