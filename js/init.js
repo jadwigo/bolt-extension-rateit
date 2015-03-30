f821c75c9df7c51f0fb4f15f3718d9240d389640
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-30 16:33:10 +0100

    Tidy JS

--- js/init.js
+++ js/init.js
@@ -1,40 +1,32 @@
-$('.rateit').bind(
-		'rated reset',
-		function(e) {
+$('.rateit').bind('rated reset', function(e) {
+	var ri = $(this);
 
-			var ri = $(this);
+	// If the user pressed reset, it will get value: 0
+	var value       = ri.rateit('value');
+	var record_id   = ri.data('bolt-record-id');
+	var contenttype = ri.data('bolt-contenttype');
 
-			// If the user pressed reset, it will get value: 0
-			var value = ri.rateit('value');
-			var record_id = ri.data('bolt-record-id');
-			var contenttype = ri.data('bolt-contenttype');
-
-			$.ajax({
-				url : asyncpath,
-				data : {
-					record_id : record_id,
-					contenttype : contenttype,
-					value : value
-				},
-				type : 'POST',
-				success : function(data) {
-					if (value != 0) {
-						var retval = data.retval;
-						var msg = data.msg;
-						$('#rateit_response').html('<span>' + msg + '</span>');
-						$('#rateit_response').show();
-					}
-
-				},
-				error : function(jxhr, msg, err) {
-					$('#rateit_response').html(
-							'<span style=\"color:red\">AJAX error: (' + err
-									+ ')</span>');
-					$('#rateit_response').show();
-				},
-				dataType : 'json'
-			});
-		});
+	$.ajax({
+		url : asyncpath,
+		data : {
+			record_id : record_id,
+			contenttype : contenttype,
+			value : value
+		},
+		type : 'POST',
+		success : function(data) {
+			var retval = data.retval;
+			var msg = data.msg;
+			$('#rateit_response-' + record_id).html('<span>' + msg + '</span>');
+			$('#rateit_response-' + record_id).show();
+		},
+		error : function(jxhr, msg, err) {
+			$('#rateit_response-' + record_id).html('<span style=\"color:red\">AJAX error: (' + err + ')</span>');
+			$('#rateit_response-' + record_id).show();
+		},
+		dataType : 'json'
+	});
+});
 
 $(document).ready(function(){
     $('.rateit').each(function(){
